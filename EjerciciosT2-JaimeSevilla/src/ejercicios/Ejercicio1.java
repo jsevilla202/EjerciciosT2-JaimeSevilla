a532ada6a0d03d512bcba089ade5114e4e548757
jsevilla <jsevilla@106-19.info2>
2022-10-18 11:05:57 +0200

    EjerciciosT2-JaimeSevilla

--- EjerciciosT2-JaimeSevilla/src/ejercicios/Ejercicio1.java
+++ EjerciciosT2-JaimeSevilla/src/ejercicios/Ejercicio1.java
@@ -1,10 +1,52 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numero;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserte un numero entre 0 al 9999: ");
+		numero = sc.nextInt();
+		
+		if(numero<0&&numero>9999) {
+			System.out.println("El número introducido no es valido");
+		}
+		
+		else if(numero<9) {
+			System.out.println("Su número es capicúa");
+		}
+		
+		else if(numero<99) {
+			if(numero%11==0) {
+				System.out.println("Su número es capicúa");
+			}
+			else {
+				System.out.println("Su número no es capicúa");
+			}
+		}
+		
+		else if(numero<999){
+			if(numero%10==numero/100) {
+				System.out.println("Su número es capicúa");
+			}
+			else {
+				System.out.println("Su número no es capicúa");
+			}
+		}
+		
+		else if(numero<9999) {
+			if(numero/100==numero%100) {
+				System.out.println("Su número es capicúa");
+			}
+			else {
+			System.out.println("Su número no es capicúa");
+			}
+		}
+		
+		sc.close();
 	}
-
 }
