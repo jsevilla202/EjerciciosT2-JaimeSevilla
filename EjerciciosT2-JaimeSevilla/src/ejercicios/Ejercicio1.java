bc8ccc8588000757ebe5c31b2fc7ec5dd971aa30
jsevilla <jsevilla@106-19.info2>
2022-10-18 12:45:43 +0200

    EjerciciosT2-JaimeSevilla

--- EjerciciosT2-JaimeSevilla/src/ejercicios/Ejercicio1.java
+++ EjerciciosT2-JaimeSevilla/src/ejercicios/Ejercicio1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		int numero;
+		int numero, decimal1, decimal2, decimal3, decimal4;
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -16,11 +16,11 @@ public class Ejercicio1 {
 			System.out.println("El número introducido no es valido");
 		}
 		
-		else if(numero<9) {
+		else if(numero<=9) {
 			System.out.println("Su número es capicúa");
 		}
 		
-		else if(numero<99) {
+		else if(numero<=99) {
 			if(numero%11==0) {
 				System.out.println("Su número es capicúa");
 			}
@@ -29,7 +29,7 @@ public class Ejercicio1 {
 			}
 		}
 		
-		else if(numero<999){
+		else if(numero<=999){
 			if(numero%10==numero/100) {
 				System.out.println("Su número es capicúa");
 			}
@@ -38,15 +38,18 @@ public class Ejercicio1 {
 			}
 		}
 		
-		else if(numero<9999) {
-			if(numero/100==numero%100) {
+		else{
+			decimal1 = numero/1000;
+			decimal3 = (numero/10)%10;
+			decimal2 = decimal3%10;
+			decimal4 = numero%10;
+			if(numero/100==numero%100||(decimal1==decimal4&&decimal2==decimal3)) {
 				System.out.println("Su número es capicúa");
 			}
 			else {
 			System.out.println("Su número no es capicúa");
 			}
 		}
-		
 		sc.close();
 	}
 }
---
+++ EjerciciosT2-JaimeSevilla/src/ejercicios/Ejercicio2.java
@@ -0,0 +1,52 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		int dni, letra;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduzca su DNI sin letra porfavor: ");
+		dni = sc.nextInt();
+		
+		letra = dni%23;
+		
+		if(dni<1||dni>99999999) {
+			System.out.println("El DNI introducido no es valido");
+		}
+		else {
+			switch(letra) {
+			case 0: System.out.println("Su letra es T"); break;
+			case 1: System.out.println("Su letra es R"); break;
+			case 2: System.out.println("Su letra es W"); break;
+			case 3: System.out.println("Su letra es A"); break;
+			case 4: System.out.println("Su letra es G"); break;
+			case 5: System.out.println("Su letra es M"); break;
+			case 6: System.out.println("Su letra es Y"); break;
+			case 7: System.out.println("Su letra es F"); break;
+			case 8: System.out.println("Su letra es P"); break;
+			case 9: System.out.println("Su letra es D"); break;
+			case 10: System.out.println("Su letra es X"); break;
+			case 11: System.out.println("Su letra es B"); break;
+			case 12: System.out.println("Su letra es N"); break;
+			case 13: System.out.println("Su letra es J"); break;
+			case 14: System.out.println("Su letra es Z"); break;
+			case 15: System.out.println("Su letra es S"); break;
+			case 16: System.out.println("Su letra es Q"); break;
+			case 17: System.out.println("Su letra es V"); break;
+			case 18: System.out.println("Su letra es H"); break;
+			case 19: System.out.println("Su letra es L"); break;
+			case 20: System.out.println("Su letra es C"); break;
+			case 21: System.out.println("Su letra es K"); break;
+			case 22: System.out.println("Su letra es E"); break;
+			}
+		}
+		
+	sc.close();
+
+	}
+
+}
